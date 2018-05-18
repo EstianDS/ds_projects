4141125594e08675196630849d4b04505feabf7c
Estian Crous <e.stian@live.co.za>
2018-05-18 22:51:56 +0200

    Committing latest changes

--- kaggle_coursera_sales_prediction/sales_prediction_model.R
+++ kaggle_coursera_sales_prediction/sales_prediction_model.R
@@ -5,9 +5,18 @@ require(tidyverse)
 # Load training data
 train_tbl <- as.tbl(read.csv("sales_train.csv"))
 
+# Load test data
+submission_tbl <- as.tbl(read.csv("test.csv"))
+
 # Load shop data
 shop_tbl <- as.tbl(read.csv("shops.csv"))
 item_tbl <- as.tbl(read.csv("items.csv"))
 item_cat_tbl <- as.tbl(read.csv("item_categories.csv"))
 
 ### Define some cleansing and transformation functions
+
+
+### Data exploration + visualization
+str(train_tbl)
+summary(train_tbl)
+
