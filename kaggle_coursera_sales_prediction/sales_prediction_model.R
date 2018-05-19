5bce3b375439150599be1702c6c9e234e1b8b26e
Estian Crous <e.stian@live.co.za>
2018-05-19 23:35:40 +0200

    Added some data visualisations

--- kaggle_coursera_sales_prediction/sales_prediction_model.R
+++ kaggle_coursera_sales_prediction/sales_prediction_model.R
@@ -13,10 +13,66 @@ shop_tbl <- as.tbl(read.csv("shops.csv"))
 item_tbl <- as.tbl(read.csv("items.csv"))
 item_cat_tbl <- as.tbl(read.csv("item_categories.csv"))
 
-### Define some cleansing and transformation functions
-
-
 ### Data exploration + visualization
 str(train_tbl)
 summary(train_tbl)
+
+###### Get and idea of sales by shop
+train_tbl %>% 
+  ##### Group by shop and date block
+  group_by(shop_id,date_block_num) %>%
+  summarise(item_cnt_day=sum(item_cnt_day)) %>% 
+  #### Group by shopid
+  group_by(shop_id) %>% 
+  summarise(item_cnt_day=sum(item_cnt_day),
+            month_count=n()) %>%
+  ##### Some shops have only been open for a very limited amount of months, 
+  ##### and naturally they vary a lot in terms of sales
+  ggplot(aes(x=month_count,y=item_cnt_day))+geom_point()+ggtitle("Store sales behaviour")
+
+####### Get an idea of sales for each item
+train_tbl %>% 
+  ##### Group by item_id and date block
+  group_by(item_id,date_block_num) %>%
+  summarise(item_cnt_day=sum(item_cnt_day)) %>% 
+  #### Group by item_id
+  group_by(item_id) %>% 
+  summarise(item_cnt_day=sum(item_cnt_day),
+            month_count=n()) %>%
+  arrange(-item_cnt_day) %>% 
+  ungroup() %>% 
+  mutate(item_rank=c(1:n())) %>% 
+  mutate(percentage=item_cnt_day/sum(item_cnt_day)) %>% 
+  mutate(cum_percentage=cumsum(percentage)) %>% 
+  gather(key,value,percentage,cum_percentage) %>% 
+  ###### One big outlier, but other than that sales are more uniformly distributed accross items than I have ever seen in practice
+  ggplot(aes(x=item_rank,y=value))+geom_col()+facet_grid(key~.,scales = "free")+ggtitle("Item Volume Distribution")#+coord_cartesian(xlim = c(0,100))
+
+
+train_tbl %>% 
+  ##### Group by item_id and date block
+  group_by(item_id,date_block_num) %>%
+  summarise(item_cnt_day=sum(item_cnt_day)) %>% 
+  #### Group by item_id
+  group_by(item_id) %>% 
+  summarise(item_cnt_day=sum(item_cnt_day),
+            month_count=n()) %>% 
+  group_by(month_count) %>% 
+  summarise(item_count=n()) %>% 
+  ungroup() %>% 
+  mutate(percentage=item_count/sum(item_count)) %>% 
+  mutate(cum_percentage=cumsum(percentage)) %>%
+  gather(key,value,percentage,cum_percentage) %>% 
+  ###### 50% of items have less than a years worth of sales data, seasonality will be difficult
+  ggplot(aes(x=month_count,y=value))+geom_col()+facet_grid(key~.,scales = "free")+ggtitle("Item Sales Life Distribution")
+  
+
+
+### Define some cleansing and transformation functions
+
+
+
+
+
+
 
